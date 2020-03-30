f747d8f3f9b34d50724f3d07dc9fb4b286a1cce8
Hubert Heijkers <hubert.heijkers@nl.ibm.com>
2020-03-30 22:57:37 +0200

    Updates

---
+++ precache-manifest.91548c81e88dfe021ae969cce701556d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a1bcdd6e5da89eda90d",
+    "url": "/carbon-tutorial/static/js/runtime~main.966ee954.js"
+  },
+  {
+    "revision": "7441f313be736475fb6a",
+    "url": "/carbon-tutorial/static/js/main.b2065fb2.chunk.js"
+  },
+  {
+    "revision": "b7e9b1e45b817cadc55a",
+    "url": "/carbon-tutorial/static/js/2.09f11355.chunk.js"
+  },
+  {
+    "revision": "7441f313be736475fb6a",
+    "url": "/carbon-tutorial/static/css/main.7fa7b3f4.chunk.css"
+  },
+  {
+    "revision": "8f59e37e36d42d90e6e5e432630d576f",
+    "url": "/carbon-tutorial/index.html"
+  }
+];
